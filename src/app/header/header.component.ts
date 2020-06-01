36a14f02cb664d2fd164fffadc13ad7ebb79ba52
Álvaro Zájara <alzca1@gmail.com>
2020-06-01 18:02:34 +0200

    added full bootstrap navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,47 +16,6 @@ import { bindCallback } from 'rxjs';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  animations: [
-    trigger('collapseNav', [
-      state(
-        'closed',
-        style({
-          height: '0px',
-          opacity: 0,
-        })
-      ),
-      state(
-        'open',
-        style({
-          height: '400px',
-          opacity: 1,
-        })
-      ),
-
-      transition('closed => open', animate('500ms ease-in-out')),
-      transition('open => closed', animate('500ms ease-in')),
-    ]),
-    trigger('collapseMenu', [
-      state(
-        'initial',
-        style({
-          height: '0',
-          overflow: 'hidden',
-          opacity: '1',
-        })
-      ),
-      state(
-        'final',
-        style({
-          height: '100px',
-          overflow: 'hidden',
-          opacity: '1',
-        })
-      ),
-      transition('initial => final', animate('300ms ease-out')),
-      transition('final => initial', animate('450ms ease-in')),
-    ]),
-  ],
 })
 export class HeaderComponent implements OnInit {
   faHamburger = faHamburger;
